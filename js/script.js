c97e2067b195f90ab4ff030c469bf285d7553680
nxlim-00 <niluza20@gmail.com>
2024-05-03 19:33:17 +0200

    Added for loop and if condition
    Added for loop to display name and height of pokemon and added comment to the heavy ones

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 let pokemonList = [
-    {name: "Snorlax", weight: 460, types: ['normal']},
-    {name: "Charizard", weight: 90, types: ['fire', 'flying']},
-    {name: "Jigglypuff", weight: 5, types: ['fairy', 'normal']}
+  {
+    name: "Snorlax", 
+    weight: 460, 
+    types: ['normal']
+  },
+  {
+    name: "Charizard", 
+    weight: 90, 
+    types: ['fire', 'flying']
+  },
+  {
+    name: "Jigglypuff", 
+    weight: 5, 
+    types: ['fairy', 'normal']
+  }
 ]
+
+
+for (let i = 0; i < pokemonList.length; i++) { // starting the array at position 0 
+    let pokemon = pokemonList[i]; // pokemon is the name of each object in the array
+    
+    document.write(`<p>${pokemon.name} (weight: ${pokemon.weight})`); // looping through the name and the weight of each pokemon (object)
+
+    if (pokemon.weight > 100) {
+        document.write(" - That's a heavy pokemon!");
+    }
+    
+    document.write("<p>");
+}
+
