bf46430967005ae370667f8b8fac6d63d5b92aa6
nxlim-00 <niluza20@gmail.com>
2024-05-09 10:38:44 +0200

    Update script.js by adding functions
    Added IIFE
    Added function getAll, function add
    Updated forEach()

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
-let pokemonList = [
-  {
-    name: "Snorlax", 
-    weight: 460, 
-    types: ['normal']
-  },
-  {
-    name: "Charizard", 
-    weight: 90, 
-    types: ['fire', 'flying']
-  },
-  {
-    name: "Jigglypuff", 
-    weight: 5, 
-    types: ['fairy', 'normal']
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Snorlax", 
+      weight: 460, 
+      types: ['normal']
+    },
+    {
+      name: "Charizard", 
+      weight: 90, 
+      types: ['fire', 'flying']
+    },
+    {
+      name: "Jigglypuff", 
+      weight: 5, 
+      types: ['fairy', 'normal']
+    }
+  ];
+
+function getAll() {
+  return pokemonList;
   }
-]
 
+function add(pokemon) {
+  pokemonList.push(pokemon);
+  }
 
-pokemonList.forEach(function(pokemon) {
+return {
+  getAll: getAll,
+  add: add
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
   document.write(pokemon.name + " (weight: " + pokemon.weight + ")");
 
   if (pokemon.weight > 100) {
