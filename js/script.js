a71514288c75fca383a42579c626494eed98703e
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-05-17 19:08:37 +0200

    Update script.js
    Deleted array of pokemonList
    Added function loadList
    Added function loadDetails
    Updated function showDetails
    Updated function add

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: "Snorlax", 
-      weight: 460, 
-      types: ['normal']
-    },
-    {
-      name: "Charizard", 
-      weight: 90, 
-      types: ['fire', 'flying']
-    },
-    {
-      name: "Jigglypuff", 
-      weight: 5, 
-      types: ['fairy', 'normal']
-    }
-  ];
- 
-/*let result = pokemonRepository.filter((pokemon) => pokemon.length > 6);
-console.log(result); */
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=150';
 
 function getAll() {
   return pokemonList;
@@ -27,18 +9,13 @@ function getAll() {
 function add(pokemon) {
   if (
     typeof pokemon === "object" &&
-    "name" in pokemon &&
-    "weight" in pokemon &&
-    "types" in pokemon
+    "name" in pokemon // &&
+    // "detailsUrl" in pokemon
   ) {
     pokemonList.push(pokemon);
   } else {
     console.log("Invalid pokemon object");
   }
-}
-  
-function showDetails(pokemon) {
-  console.log(pokemon)
 }
 
 // Function add list item to DOM
@@ -57,22 +34,68 @@ function addListItem(pokemon) {
   // append button to list item
   listPokemon.appendChild(button);
   pokemonList.appendChild(listPokemon);
+
+  // eventListener button to show details
   button.addEventListener("click", function(){
     showDetails(pokemon);
   });
-};
+}
+
+// promise function to load list of pokemons
+async function loadList() {
+  return fetch(apiUrl).then(function (response) { // get  
+    return response.json(); // convert response to json => all the data from the apiUrl
+  }).then(function (json) {
+
+    /* "for each of this item create a pokemon and turn that pokemon into an object with two keys and two values" */
+    json.results.forEach(function (item) { 
+    /* 'results' is on data page of the api -- in this example: results contains an array full of pokemon */
+
+      let pokemon = {
+        name: item.name, // item is parameter of function and name + url is from api
+        detailsUrl: item.url
+      };
+      add(pokemon);
+      console.log(pokemon);
+    });
+  }).catch(function (e) {
+    console.error(e);
+  })
+}
+
+// load details of pokemon
+async function loadDetails (item) {
+  let url = item.detailsUrl; // detailsUrl defined in loadList function
+  try {
+    const response = await fetch(url);
+    const details = await response.json();
+    // adding details to items
+    item.imageUrl = details.sprites.front_default; // sprites.front_default = item in api and defines the preferred perspective of pokemon
+    item.height = details.height;
+    item.types = details.types;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function showDetails(pokemon) {
+  loadDetails(pokemon).then(function () {
+    console.log(pokemon)
+  });
+}
 
 return {
   getAll: getAll,
   add: add,
   addListItem: addListItem,
-  showDetails: showDetails
+  showDetails: showDetails,
+  loadList: loadList,
+  loadDetails: loadDetails
   };
 })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({name: "Charmander", weight: 8.5, types: ['fire']});
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() { 
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
